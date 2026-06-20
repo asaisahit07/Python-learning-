dcfef6b3c431a478f53b432e3035b90a0af7f95c
asaisahit07 <asaisahit07@gmail.com>
2026-06-20 23:35:09 +0530

    Create day1_basics.py
    add day 1 python basics

---
+++ day1_basics.py
@@ -0,0 +1,41 @@
+#print
+print("first program- python print function")
+print("it is declared like this :")
+print("printf(what to print)")
+
+#string concatenation
+print("string manipulatio exercise")
+print("string concatenation is done with "+" sign")
+print("hello\t"+"world")
+
+#input function
+print("hey "+input("what is your name?")+", how are you?")
+
+#varibale declaration
+name=input("what is your name.? ")
+length=len(name)
+print(length)
+
+#swapping of two numbrs
+print("before swappping")
+a=input("enter the value of a=")
+b=input("enter the value of b=")
+temp=a
+a=b
+b=temp
+print("after swapping")
+print("a="+a)
+print("b="+b)
+
+#data types , to check the data type of a variable we use type() function
+var1=10
+var2=29.3453456
+print(var1+var2)
+print(type(var1))
+print(type(var2))
+
+#boolean data type
+a=2
+b=1
+var=a<b
+print(var)
